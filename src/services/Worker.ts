b73aa89e0c6d8cb39738ff47801caf537791e77c
francis <franzdiaz460@gmail.com>
2024-08-11 13:47:58 +0400

    ref: creating multiple threads to crawl

--- src/services/Worker.ts
+++ src/services/Worker.ts
@@ -1,5 +1,21 @@
 import path from "path";
-const { Worker } = require("worker_threads");
+import { Worker } from "worker_threads";
+
+const THREAD_POOL = 3;
 
 const worker_file = path.join(__dirname, "./Crawler/index.ts");
-const worker = new Worker(worker_file);
+
+export default class WorkerThread {
+  webpages: Array<string> = [];
+
+  constructor(webpages: Array<string>) {
+    this.webpages = webpages;
+    this.init();
+  }
+
+  private init() {
+    for (let i = 0; i < this.webpages.length; i++) {
+      const worker = new Worker(worker_file, { argv: [this.webpages[i]] });
+    }
+  }
+}
