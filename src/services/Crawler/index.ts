baeb7fc3cdee3d0f2ab81ee0bd191c1dfac61097
francis <franzdiaz460@gmail.com>
2024-08-12 04:38:05 +0400

    ref: created classes for crwling and creating an instance of a headless browser to extract data from.

--- src/services/Crawler/index.ts
+++ src/services/Crawler/index.ts
@@ -1,17 +1,40 @@
-class Crawler {
-  webpage: string;
-  constructor(webpage: string) {
-    this.webpage = webpage;
-  }
+import puppeteer, { Puppeteer } from "puppeteer";
 
-  async start_crawl() {
-    this.crawl(this.webpage);
+class Scraper {
+  private link: string;
+  constructor() {
+    this.link = "";
   }
-
-  private async crawl(link: string) {
-    console.log(`crawl: ${link}`);
+  async launch_browser() {
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+    await page.goto(this.link);
+    const test = await page.$(".banner");
+    console.log(test);
+  }
+  set(new_link: string) {
+    this.link = new_link;
+    return new_link;
   }
 }
 
-const crawler = new Crawler(process.argv[2]);
-crawler.start_crawl();
+// when index_page is triggered, store processed data on a data structure
+
+class Crawler {
+  private scraper: Scraper;
+  constructor(Scraper: { new (): Scraper }) {
+    this.scraper = new Scraper();
+  }
+  async start_crawl(link: string) {
+    this.crawl(link);
+  }
+  private async crawl(link: string) {
+    this.scraper.set(link);
+    this.scraper.launch_browser();
+    console.log(`crawl: ${link}`);
+  }
+  private async index_page() {}
+}
+
+const crawler = new Crawler(Scraper);
+crawler.start_crawl(process.argv[2]);
