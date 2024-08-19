7b4a96fbafe345048cfce7f043a263fd0295e85f
francis <franzdiaz460@gmail.com>
2024-08-19 19:09:59 +0400

    ref: deserializing object from a thread.

--- src/services/ThreadHandler.ts
+++ src/services/ThreadHandler.ts
@@ -5,6 +5,7 @@ import WebsiteDatabase from "./DB";
 import { arrayBuffer } from "stream/consumers";
 const db = new WebsiteDatabase().init_database();
 const THREAD_POOL = 3;
+const BUFFER_SIZE = 2048;
 const worker_file = path.join(__dirname, "./Bot/index.ts");
 
 type thread_response_t = {
@@ -19,15 +20,19 @@ export default class ThreadHandler {
     this.webpages = webpages;
     this.crawl_and_index();
   }
-  private event_handlers(worker: Worker) {
+
+  private event_handlers(worker: Worker, shared_buffer: ArrayBufferLike) {
     console.log(`WorkerID: ${worker.threadId}`);
     worker.on("message", (message: thread_response_t) => {
       if (message.type === "error") {
-        console.log("Thread #%s Threw an error: ");
+        console.log("Thread s% Threw an error: ", worker.threadId);
       }
+
       if (message.type === "insert") {
+        if (message.shared_buffer == undefined) return;
+        this.message_decoder(message.shared_buffer);
         console.log(
-          "Thread #%s changed buffer: ",
+          "Thread %s changed buffer: ",
           worker.threadId,
           message.shared_buffer,
         );
@@ -37,21 +42,28 @@ export default class ThreadHandler {
       console.log(`Worker Exit`);
     });
   }
-  private message_decoder(message: ArrayBufferLike) {
+
+  private message_decoder(thread_buffer: ArrayBufferLike) {
     const decoder = new TextDecoder();
-    const decoded_data = decoder.decode(message);
+    const decoded_data = decoder.decode(thread_buffer);
+    const last_brace_index = decoded_data.lastIndexOf("}");
+    const sliced_object = decoded_data.slice(0, last_brace_index + 1);
+
+    const deserialize_data = JSON.parse(sliced_object);
+    console.log({ decoded_data });
+    console.log(deserialize_data);
   }
 
   private crawl_and_index() {
-    const array_buffer = new SharedArrayBuffer(20);
+    const array_buffer = new SharedArrayBuffer(BUFFER_SIZE);
     const shared_buffer = new Uint8Array(array_buffer);
     try {
-      for (let i = 0; i < 2; i++) {
+      for (let i = 0; i < 1; i++) {
         const worker = new Worker(worker_file, {
           argv: [i],
           workerData: { shared_buffer },
         });
-        this.event_handlers(worker);
+        this.event_handlers(worker, shared_buffer);
       }
     } catch (err) {
       const error = err as Error;
