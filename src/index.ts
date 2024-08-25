891c5bda0edf6feae0d34627cdc70636305efe1d
francis <franzdiaz460@gmail.com>
2024-08-25 19:06:23 +0400

    ref: added RabbitMq message broker to notify crawler for crawling event

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import express, { Request, Response, NextFunction } from "express";
 import path from "path";
-
+import amqp, { Connection } from "amqplib/callback_api";
+const cors = require("cors");
+const body_parser = require("body-parser");
 const app = express();
 const PORT = 8080;
 
@@ -8,17 +10,48 @@ app.listen(PORT, () => {
   console.log("Listening to Port:", PORT);
 });
 
+app.use(body_parser.urlencoded({ extended: false }));
+app.use(body_parser.json());
+app.use(cors());
 app.use(express.static(path.join(__dirname, "public")));
-
 app.use(
   express.static(path.join(__dirname, "public/scripts")),
-  (req: Request, res: Response) => {
-    console.log(req.originalUrl);
+  (req: Request, res: Response, next: NextFunction) => {
     if (req.path.endsWith(".js")) {
       res.setHeader("Content-Type", "application/javascript");
+      next();
     }
+    next();
   },
 );
+
 app.get("/", (req: Request, res: Response) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.post("/crawl", (req: Request, res: Response) => {
+  console.log("Crawl");
+
+  amqp.connect("amqp://localhost", (err, connection: Connection) => {
+    if (err) throw err;
+    connection.createChannel((err, channel) => {
+      if (err) throw err;
+      const queue = "hello";
+      const message = "hello world";
+      channel.assertQueue(queue, {
+        durable: false,
+      });
+      channel.sendToQueue(queue, Buffer.from(message));
+      console.log("[x] Sent %s", message);
+      //setTimeout(function () {
+      //  connection.close();
+      //}, 500);
+      res.send("<p>Crawling...</p>");
+    });
+  });
+});
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  console.error(err.stack);
+  res.status(500).send("Something went wrong!");
 });
