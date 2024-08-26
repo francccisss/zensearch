c56ecd13e0f48f9ec770054a674d2770fa2d5df1
francis <franzdiaz460@gmail.com>
2024-08-26 10:49:28 +0400

    ref: added loading state for crawling event

--- src/public/scripts/index.js
+++ src/public/scripts/index.js
@@ -1,1 +1,9 @@
 console.log("Initialized Script");
+
+const crawl_btn = document.getElementById("crawl-btn");
+
+crawl_btn.addEventListener("click", (e) => {
+  const target = e.currentTarget;
+  target.textContent = "This might take a while...";
+  target.disabled = true;
+});
