48ecb1f0f918c27a73f82757a757d7464964f8d8
francis <franzdiaz460@gmail.com>
2024-08-27 18:42:40 +0400

    ref: able to respond back to client if there are no messages in the message queue this is to handle if clients sends a polling request and the worker queue is empty due to the service processing the current job

--- src/public/scripts/index.js
+++ src/public/scripts/index.js
@@ -1,3 +1,9 @@
+"use strict";
+import poll_current_job from "./job_polling.js";
+(async () => {
+  if (document.cookie === "") return;
+  await poll_current_job();
+})();
 console.log("Initialized Script");
 
 const crawl_btn = document.getElementById("crawl-btn");
