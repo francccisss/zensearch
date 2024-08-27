cdc5ea189bc0e9c82740fc0419e67f3707d79d27
francis <franzdiaz460@gmail.com>
2024-08-27 19:56:53 +0400

    fix: needed to create different channels for pushing and consuming messages from differet queues, and close them right after to prevent duplicate requests to the same message queue after client recevies the crawled dataset

--- src/index.ts
+++ src/index.ts
@@ -45,7 +45,9 @@ app.post("/crawl", async (req: Request, res: Response, next: NextFunction) => {
     const queue = "crawl_rpc_queue";
     const message = "Start Crawl";
     const corID = "f27ac58-7bee-4e90-93ef-8bc08a37e26c";
-    const { connection, channel } = await connect_rabbitmq();
+    const connection = await connect_rabbitmq();
+    if (connection === null) throw new Error("TCP Connection lost.");
+    const channel = await connection.createChannel();
     const response_queue = await channel.assertQueue("polling_worker_queue", {
       exclusive: false,
     });
@@ -53,6 +55,8 @@ app.post("/crawl", async (req: Request, res: Response, next: NextFunction) => {
       replyTo: response_queue.queue,
       correlationId: corID,
     });
+
+    await channel.close();
     res.cookie("job_id", corID);
     res.cookie("job_queue", response_queue.queue);
     res.send("<p>Crawling...</p>");
@@ -68,34 +72,37 @@ app.get("/job", async (req: Request, res: Response, next: NextFunction) => {
   const { job_id, job_queue } = req.query;
   if (job_id === undefined || job_queue === undefined)
     throw new Error("There's no worker queue for this session for crawling.");
-  console.log(job_id, job_queue);
-  console.log("Check Worker queue");
+
   try {
-    const { connection, channel } = await connect_rabbitmq();
+    const connection = await connect_rabbitmq();
+    if (connection === null) throw new Error("TCP Connection lost.");
+    const channel = await connection.createChannel();
     const { queue, messageCount, consumerCount } = await channel.checkQueue(
       job_queue as string,
     );
     if (messageCount === 0) {
       res.send("Crawling...");
+    } else {
+      const consumer = await channel.consume(
+        job_queue as string,
+        async (response) => {
+          if (response === null) throw new Error("No Response");
+          if (response.properties.correlationId === job_id) {
+            console.log(
+              "LOG: Response from crawler received: %s",
+              response.content.toString(),
+            );
+            console.log("CONSUMED");
+            res.clearCookie("job_id");
+            res.clearCookie("job_queue");
+            res.send("Success");
+            await channel.close();
+            //channel.cancel(consumer.consumerTag).catch(console.error);
+          }
+        },
+        { noAck: true },
+      );
     }
-    const consumer = await channel.consume(
-      job_queue as string,
-      (response) => {
-        if (response === null) throw new Error("No Response");
-        if (response.properties.correlationId === job_id) {
-          console.log(
-            "LOG: Response from crawler received: %s",
-            response.content.toString(),
-          );
-          console.log("CONSUMED");
-          res.clearCookie("job_id");
-          res.clearCookie("job_queue");
-          res.send("<p>Success</p>");
-          channel.cancel(consumer.consumerTag).catch(console.error);
-        }
-      },
-      { noAck: true },
-    );
   } catch (err) {
     const error = err as Error;
     console.log("LOG:Something went wrong with RPC queue");
