318fe8a91118b2522cac74e81fa62a3f59532956
francis <franzdiaz460@gmail.com>
2024-08-27 19:56:53 +0400

    fix: needed to create different channels for pushing and consuming messages from differet queues, and close them right after to prevent duplicate requests to the same message queue after client recevies the crawled dataset

--- src/rabbitmq.ts
+++ src/rabbitmq.ts
@@ -1,17 +1,15 @@
 import amqp, { Connection, Channel } from "amqplib";
 let connection: Connection | null = null;
-let channel: Channel | null = null;
-async function connect_rabbitmq() {
-  if (connection && channel) {
-    return { connection, channel };
+async function connect_rabbitmq(): Promise<Connection | null> {
+  if (connection) {
+    return connection;
   }
   try {
     connection = await amqp.connect("amqp://localhost");
-    channel = await connection.createChannel();
 
     console.log("Connected to RabbitMQ");
 
-    return { connection, channel };
+    return connection;
   } catch (error) {
     console.error("Failed to connect to RabbitMQ:", error);
     throw error;
