787302ff2520552b37c6373786a668b86d4e46c3
francis <franzdiaz460@gmail.com>
2024-08-29 13:39:38 +0400

    ref: created a search input field and fixed spinner

--- gateway/src/public/scripts/index.js
+++ gateway/src/public/scripts/index.js
@@ -3,20 +3,27 @@ import polling from "./job_polling.js";
 import PubSub from "./pubsub.js";
 
 const crawl_btn = document.getElementById("crawl-btn");
+const crawl_btn_container = document.getElementById("crawl-btn").parentElement;
 const POLL_INTERVAL = 3;
 const spinner = document.querySelector("span.process-spinner");
+const search_bar = document.querySelector('input[type="search"]');
 const pubsub = new PubSub();
 let is_crawling = false;
 
 pubsub.subscribe("polling_event", handle_on_crawl);
 
+// to continue polling when users refresh the page
 (async () => {
   if (document.cookie !== "") {
     crawl_btn.disabled = true;
+    crawl_btn_container.classList.add("polling");
+    search_bar.readOnly = true;
     crawl_btn.textContent = "Crawling...";
   }
   await polling.poll_loop();
   crawl_btn.disabled = false;
+  crawl_btn_container.classList.remove("polling");
+  search_bar.readOnly = false;
 })();
 
 async function handle_on_crawl() {
@@ -27,14 +34,20 @@ async function handle_on_crawl() {
     });
     await polling.poll_loop();
     crawl_btn.disabled = false;
+    search_bar.readOnly = false;
+    crawl_btn_container.classList.remove("polling");
   } catch (err) {
+    crawl_btn.disabled = false;
+    search_bar.readOnly = false;
     console.error(err.message);
   }
 }
 
 crawl_btn.addEventListener("click", async (e) => {
   const target = e.currentTarget;
+  search_bar.readOnly = true;
   target.disabled = true;
   target.textContent = "Crawling...";
+  crawl_btn_container.classList.add("polling");
   pubsub.publish("polling_event");
 });
--- gateway/src/public/scripts/ui.js
+++
@@ -1,9 +0,0 @@
-function set_btn_processing(spinner, is_processing) {
-  if (is_processing) {
-    spinner.style.opacity = 1;
-    return;
-  }
-  spinner.style.opacity = 0;
-}
-
-export default { set_btn_processing };
