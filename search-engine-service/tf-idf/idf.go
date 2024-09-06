d2142edaf20ab8c978bd6c7cd10497549107eb7d
francis <franzdiaz460@gmail.com>
2024-09-06 08:27:23 +0400

    fix: word document going out bounds from iterating over content string

--- search-engine-service/tf-idf/idf.go
+++ search-engine-service/tf-idf/idf.go
@@ -1,8 +1,38 @@
 package tfidf
 
-import "search-engine-service/utilities"
+import (
+	"fmt"
+	"math"
+	"search-engine-service/utilities"
+	"strings"
+)
 
-func CalculateIDF(searchQuery string, webpage *utilities.WebpageTFIDF) float32 {
+func CalculateIDF(searchQuery string, webpages []utilities.WebpageTFIDF) float64 {
 
-	return 0.0
+	totalWords := float64(wordCountInCorpa(webpages))
+	totalTermsInDocument := float64(termCountInDocuments(searchQuery, webpages))
+
+	fmt.Printf("Total words in corpa: %f\n", totalWords)
+	fmt.Printf("Total terms in corpa: %f\n", totalWords)
+	fmt.Printf("Calculated IDF: %f\n", math.Log2(totalTermsInDocument/totalWords))
+
+	return math.Log2(totalTermsInDocument / totalWords)
+}
+
+func termCountInDocuments(term string, corpa []utilities.WebpageTFIDF) int {
+	termCount := 0
+	for _, webpage := range corpa {
+		termCount += strings.Count(webpage.Contents, term)
+	}
+	return termCount
+}
+
+func wordCountInCorpa(corpa []utilities.WebpageTFIDF) int {
+	wordCount := 0
+	for _, webpage := range corpa {
+		webpageWords := utilities.DocumentWordCount(webpage.Contents)
+		wordCount += webpageWords
+	}
+	return wordCount
+
 }
