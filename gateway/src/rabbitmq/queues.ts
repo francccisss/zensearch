d628c4edd3a262881ed64309b94947fe26df231a
francis <franzdiaz460@gmail.com>
2024-09-12 07:51:02 +0400

    ref: creating a search query poll instead of using websocket to reduce complexity

---
+++ gateway/src/rabbitmq/queues.ts
@@ -0,0 +1,5 @@
+const CRAWL_QUEUE = "crawl_queue";
+const CRAWL_QUEUE_CB = "crawl_poll_queue";
+const SEARCH_QUEUE_CB = "search_poll_queue";
+
+export { CRAWL_QUEUE, CRAWL_QUEUE_CB, SEARCH_QUEUE_CB };
