0a505f8f85d48af9d9bfbaf9651727a3af24324f
francis <franzdiaz460@gmail.com>
2024-09-16 17:08:52 +0400

    ref: setting up channel listeners

--- web-crawler-service-golang/main.go
+++ web-crawler-service-golang/main.go
@@ -1,9 +1,61 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"encoding/json"
+	amqp "github.com/rabbitmq/amqp091-go"
+	"log"
+)
 
-// TODO initalize rabbitmq to listen for push queues
+const crawlQueue = "crawl_queue"
+
+type Webpages struct {
+	Webpages []Webpage
+}
+
+type Webpage struct {
+	Title       string
+	Contents    string
+	Webpage_url string
+}
 
 func main() {
-	fmt.Printf("Hello")
+
+	var (
+		ctx context.Context
+	)
+
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	if err != nil {
+		log.Panicf("Unable to establish a tcp connection with message broker.")
+	}
+	defer conn.Close()
+
+	crawlChannel, err := conn.Channel()
+	if err != nil {
+		log.Panicf("Unable to create a crawl channel.")
+	}
+	crawlChannel.QueueDeclare(crawlQueue, false, false, false, false, nil)
+
+	aliveMainThread := make(chan struct{})
+	go channelHandler(ctx, crawlChannel)
+	<-aliveMainThread
+}
+
+func channelHandler(ctx context.Context, chann *amqp.Channel) {
+
+	_, err := chann.Consume("", crawlQueue, false, false, false, false, nil)
+	if err != nil {
+		log.Panicf("Unable to create a crawl channel.")
+	}
+
+	for {
+		// select demultiplexing inputs
+		// exit go routine when some error occurs
+	}
+
+}
+
+func parseIncomingData(data []byte) {
+
 }
