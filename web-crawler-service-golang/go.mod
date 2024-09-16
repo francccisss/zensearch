4ac7ffc17bf71ce0de919fbf04c312570bfb9756
francis <franzdiaz460@gmail.com>
2024-09-16 08:13:10 +0400

    ref: initializing go service

---
+++ web-crawler-service-golang/go.mod
@@ -0,0 +1,3 @@
+module web-crawler-service
+
+go 1.22.4
---
+++ web-crawler-service-golang/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+// TODO initalize rabbitmq to listen for push queues
+
+func main() {
+	fmt.Printf("Hello")
+}
