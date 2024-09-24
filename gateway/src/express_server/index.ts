78109f9abe6a3c040ad47a5338cd9db69ac15af1
francis <franzdiaz460@gmail.com>
2024-09-24 14:00:14 +0400

    fix: headless chrome driver taking up too much process even after the program has exitted

--- gateway/src/express_server/index.ts
+++ gateway/src/express_server/index.ts
@@ -36,6 +36,8 @@ app.get("/", (req: Request, res: Response) => {
 app.post("/crawl", async (req: Request, res: Response, next: NextFunction) => {
   const Docs = [
     "https://fzaid.vercel.app/",
+    "https://robbowen.digital/",
+    "https://brittanychiang.com",
     //"https://www.mcconeproperties.com/",
   ];
 
