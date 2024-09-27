0cae1b895e0b167280d55df1925b7ccef4f35cce
francis <franzdiaz460@gmail.com>
2024-09-27 11:03:34 +0400

    ref: contained channel creation

--- gateway/src/index.ts
+++ gateway/src/index.ts
@@ -7,27 +7,28 @@ import app from "./express_server";
 const PORT = 8080;
 (async function start_server() {
   const http_server = http.createServer(app);
-  //await amqp.connect("amqp://localhost");
   /*
    Connect Rabbitmq
-   Creates an indefinite loop to listen receive
+   Creates an indefinite loop to listen/receive
    new messages from the message broker.
 
    TODO Create a class for rabbitmq
   */
   const rbq_client = await rabbitmq.client.connectClient();
+
   // Connect Websocket for search results retreival
   const wss: WebSocketServer = new WebSocketServer({ server: http_server });
   const ws_service = new WebsocketService(wss);
   ws_service.handler();
 
   /*
-   since actions from users eg: sending a crawl task,
-   polling crawled tasks, and sending search queries
-   are all handled by the express-server
-   these rabbitmq handlers are specifically used for websockets
+   Since actions from users eg: sending a crawl task,
+   polling crawled tasks, and sending search queries are all handled
+   by the express-server these rabbitmq handlers are specifically used for websockets
 
-   the search_channel_listener initializes listeners and takes in a cb
+   The `init_search_channel_queues` asserts message queues.
+
+   The `search_channel_listener` initializes listeners and takes in a cb
    function from the websocket server that pushes messages back to the client
    once the search_channel_listener consumes a message from the search engine service
    through `SEARCH_QUEUE_CB` routing key.
