790bf4397378d6cee794626efc53351444839c2f
francis <franzdiaz460@gmail.com>
2024-09-27 10:44:17 +0400

    ref: containing rabbitmq client in a class

--- gateway/src/rabbitmq/index.ts
+++ gateway/src/rabbitmq/index.ts
@@ -4,53 +4,56 @@ import {
   CRAWL_QUEUE_CB,
   SEARCH_QUEUE,
   SEARCH_QUEUE_CB,
-} from "./queues";
+} from "./routing_keys";
 let connection: Connection | null = null;
 let search_channel: Channel;
 
-async function connect(): Promise<Connection | null> {
-  if (connection) {
-    return connection;
-  }
-  try {
-    connection = await amqp.connect("amqp://localhost");
-    console.log("Connected to RabbitMQ");
-    return connection;
-  } catch (error) {
-    console.error("Failed to connect to RabbitMQ:", error);
-    throw error;
-  }
-}
+class RabbitMQClient {
+  connection: null | Connection = null;
 
-async function init_search_channel_queues() {
-  const connection = await connect();
-  if (connection === null) throw new Error("TCP Connection lost.");
-  search_channel = await connection.createChannel();
-  await search_channel.assertQueue(SEARCH_QUEUE, {
-    exclusive: false,
-    durable: false,
-  });
-  await search_channel.assertQueue(SEARCH_QUEUE_CB, {
-    exclusive: false,
-    durable: false,
-  });
-}
+  async connectClient() {
+    this.connection = await amqp.connect("amqp://localhost");
+  }
 
-/*
+  async init_search_channel_queues() {
+    try {
+      if (connection == null) {
+        throw new Error(
+          "Unable establish a tcp connection with rabbitmq server.",
+        );
+      }
+      search_channel = await connection.createChannel();
+      await search_channel.assertQueue(SEARCH_QUEUE, {
+        exclusive: false,
+        durable: false,
+      });
+      await search_channel.assertQueue(SEARCH_QUEUE_CB, {
+        exclusive: false,
+        durable: false,
+      });
+    } catch (err) {
+      console.error(
+        "ERROR: Something went wrong while creating search channels.",
+      );
+      console.error(err);
+    }
+  }
+
+  /*
   This Function sends a new search query to the SEARCH ENGINE SERVICE.
   TODO error handle this please. :D
 */
-async function send_search_query(job: {
-  q: string;
-  job_id: string;
-}): Promise<boolean> {
-  return await search_channel.sendToQueue(SEARCH_QUEUE, Buffer.from(job.q), {
-    correlationId: job.job_id,
-    replyTo: SEARCH_QUEUE_CB,
-  });
-}
+  async send_search_query(job: {
+    q: string;
+    job_id: string;
+  }): Promise<boolean> {
+    return await search_channel.sendToQueue(SEARCH_QUEUE, Buffer.from(job.q), {
+      correlationId: job.job_id,
+      replyTo: SEARCH_QUEUE_CB,
+    });
+  }
 
-/*
+  /*
   A listener for consuming search engine's search results that is pushed to the message queue
   using `CRAWL_QUEUE_CB` routing key after it finishes processing user's search query.
 
@@ -59,102 +62,100 @@ async function send_search_query(job: {
 
   TODO Handle errors in here please :D
 */
-async function search_channel_listener(
-  cb: (data: ConsumeMessage | null) => void,
-) {
-  await search_channel.consume(
-    SEARCH_QUEUE_CB,
-    async (msg: ConsumeMessage | null) => {
-      if (msg === null) throw new Error("Msg does not exist");
-      console.log(msg);
-      cb(msg);
-      search_channel.ack(msg);
-    },
-  );
-}
-
-async function poll_job(
-  chan: Channel,
-  job: { id: string; queue: string },
-): Promise<{ done: boolean; data: any }> {
-  try {
-    await chan.assertQueue(job.queue as string, {
-      exclusive: false,
-      durable: false,
-    });
-    const { queue, messageCount, consumerCount } = await chan.checkQueue(
-      job.queue as string,
-    );
-    console.log(queue);
-    console.log(messageCount);
-    console.log(consumerCount);
-    if (messageCount === 0) {
-      return { done: false, data: {} };
-    }
-    let data: any;
-    const consumer = await chan.consume(
-      job.queue as string,
-      async (response) => {
-        if (response === null) throw new Error("No Response");
-        console.log("LOG: Response from Polled Job received");
-        data = response.content.toString();
-        console.log("CONSUMED");
-        chan.ack(response);
-        return { done: true, data };
+  async search_channel_listener(cb: (data: ConsumeMessage | null) => void) {
+    await search_channel.consume(
+      SEARCH_QUEUE_CB,
+      async (msg: ConsumeMessage | null) => {
+        if (msg === null) throw new Error("Msg does not exist");
+        console.log(msg);
+        cb(msg);
+        search_channel.ack(msg);
       },
     );
-    return { done: true, data };
-  } catch (err) {
-    const error = err as Error;
-    console.log("LOG:Something went wrong while polling queue");
-    console.error(error.message);
-    throw new Error(error.message);
+  }
+
+  /*
+   * These methods underneath are used in the express server
+   * their purpose is to push messages into the message queue for every user
+   * request that is sent to the server.
+   *
+   */
+  async poll_job(
+    chan: Channel,
+    job: { id: string; queue: string },
+  ): Promise<{ done: boolean; data: any }> {
+    try {
+      await chan.assertQueue(job.queue as string, {
+        exclusive: false,
+        durable: false,
+      });
+      const { queue, messageCount, consumerCount } = await chan.checkQueue(
+        job.queue as string,
+      );
+      console.log(queue);
+      console.log(messageCount);
+      console.log(consumerCount);
+      if (messageCount === 0) {
+        return { done: false, data: {} };
+      }
+      let data: any;
+      const consumer = await chan.consume(
+        job.queue as string,
+        async (response) => {
+          if (response === null) throw new Error("No Response");
+          console.log("LOG: Response from Polled Job received");
+          data = response.content.toString();
+          console.log("CONSUMED");
+          chan.ack(response);
+          return { done: true, data };
+        },
+      );
+      return { done: true, data };
+    } catch (err) {
+      const error = err as Error;
+      console.log("LOG:Something went wrong while polling queue");
+      console.error(error.message);
+      throw new Error(error.message);
+    }
+  }
+
+  async crawl_job(
+    chan: Channel,
+    websites: Uint8Array,
+    job: { queue: string; id: string },
+  ) {
+    const message = "Start Crawl";
+    try {
+      await chan.assertQueue(CRAWL_QUEUE, {
+        exclusive: false,
+        durable: false,
+      });
+
+      await chan.assertQueue(CRAWL_QUEUE_CB, {
+        exclusive: false,
+        durable: false,
+      });
+      const success = await chan.sendToQueue(
+        CRAWL_QUEUE,
+        Buffer.from(websites.buffer),
+        {
+          replyTo: CRAWL_QUEUE_CB,
+          correlationId: job.id,
+        },
+      );
+      if (!success) {
+        throw new Error("Unable to send to job to crawl queue");
+      }
+      await chan.close();
+      return true;
+    } catch (err) {
+      const error = err as Error;
+      console.error("LOG: Something went wrong while starting the crawl.");
+      console.error(error.message);
+      await chan.close();
+      return false;
+    }
   }
 }
 
-async function crawl_job(
-  chan: Channel,
-  websites: Uint8Array,
-  job: { queue: string; id: string },
-) {
-  const message = "Start Crawl";
-  try {
-    await chan.assertQueue(CRAWL_QUEUE, {
-      exclusive: false,
-      durable: false,
-    });
-
-    await chan.assertQueue(CRAWL_QUEUE_CB, {
-      exclusive: false,
-      durable: false,
-    });
-    const success = await chan.sendToQueue(
-      CRAWL_QUEUE,
-      Buffer.from(websites.buffer),
-      {
-        replyTo: CRAWL_QUEUE_CB,
-        correlationId: job.id,
-      },
-    );
-    if (!success) {
-      throw new Error("Unable to send to job to crawl queue");
-    }
-    await chan.close();
-    return true;
-  } catch (err) {
-    const error = err as Error;
-    console.error("LOG: Something went wrong while starting the crawl.");
-    console.error(error.message);
-    await chan.close();
-    return false;
-  }
-}
-
-export default {
-  search_channel_listener,
-  connect,
-  send_search_query,
-  poll_job,
-  crawl_job,
-  init_search_channel_queues,
-};
+export default {};
