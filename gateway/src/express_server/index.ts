96274ee39eb5ef1c35e116a7bce3a750bcba7fd9
francis <franzdiaz460@gmail.com>
2024-09-30 10:01:16 +0400

    ref: returning indexed data from db service after crawling

--- gateway/src/express_server/index.ts
+++ gateway/src/express_server/index.ts
@@ -36,8 +36,8 @@ app.get("/", (req: Request, res: Response) => {
 app.post("/crawl", async (req: Request, res: Response, next: NextFunction) => {
   const Docs = [
     "https://fzaid.vercel.app/",
-    "https://robbowen.digital/",
-    "https://naren200.github.io/",
+    //"https://robbowen.digital/",
+    //"https://naren200.github.io/",
     "https://brittanychiang.com",
   ];
 
