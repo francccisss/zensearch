6c558f59f5312b8bdb8526afc110bd8397e9664a
francis <franzdiaz460@gmail.com>
2024-10-03 00:59:05 +0400

    ref: adding a crawl limit

--- gateway/src/public/scripts/index.js
+++ gateway/src/public/scripts/index.js
@@ -4,9 +4,13 @@ import ui from "./ui/index.js";
 import pubsub from "./utils/pubsub.js";
 
 const sidebar = document.getElementById("crawl-list-sb");
+const newEntry = document.querySelector(".new-entry-btn");
 const closeSbBtn = document.getElementById("close-sb");
 const openSbBtn = document.getElementById("add-entry-sb-btn");
 const listContainer = document.querySelector(".list-container");
+const limit = 6;
+const popup = document.createElement("p");
+popup.classList.add("info-large");
 
 window.addEventListener("load", () => {
   ui.init();
@@ -22,6 +26,23 @@ pubsub.subscribe("removeEntry", crawlInput.updateEntries);
 pubsub.subscribe("removeEntry", (entries) => {
   if (entries.length === 0) {
     listContainer.innerHTML = `<p class='info-large'>Seems like you don't have any entries yet, click on "New entry".</p>`;
+  }
+  if (entries.length <= limit) {
+    newEntry.disabled = false;
+    const children = Array.from(listContainer.children);
+    children.forEach((child) => {
+      if (child.classList.contains("info-large")) {
+        child.remove();
+      }
+    });
+  }
+});
+pubsub.subscribe("addEntry", (entries) => {
+  popup.textContent = "You've reached the maximum limit.";
+  if (entries.length >= limit) {
+    console.log("lol");
+    listContainer.appendChild(popup);
+    newEntry.disabled = true;
   }
 });
 pubsub.subscribe("hideEntry", crawlInput.updateEntries);
