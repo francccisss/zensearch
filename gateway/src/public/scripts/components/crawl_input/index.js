854d0b7d764ab5bb521a6711990c569a615eb088
francis <franzdiaz460@gmail.com>
2024-10-02 14:04:20 +0400

    ref: creating sidebar to contain url entries

--- gateway/src/public/scripts/components/crawl_input/index.js
+++ gateway/src/public/scripts/components/crawl_input/index.js
@@ -1,3 +1,25 @@
+const sheet = new CSSStyleSheet();
+sheet.replaceSync(`
+  button,input{
+    appearance: none;
+    border-radius: 9999px;
+    border:none;
+    padding: var(--crawl-input-btn-padding);
+    font-family:"Poppins";
+  }
+  button{
+    position: absolute;
+    left:100%;
+    top:0%;
+    background-color: var(--black);
+    color: var(--light-text);
+  }
+  input{
+    width:100%;
+    background-color: var(--input-color);
+  }
+`);
+
 class Component extends HTMLElement {
   input;
   constructor() {
@@ -7,8 +29,11 @@ class Component extends HTMLElement {
   #createComponent() {
     const shadow = this.attachShadow({ mode: "open" });
     const temp = document.getElementById("crawl-input-temp");
+    shadow.adoptedStyleSheets = [sheet];
     shadow.append(temp.content.cloneNode(true));
     this.classList.add("crawl-input");
+    this.style.position = "relative";
+    //this.style.width = "70%";
   }
 }
 customElements.define("crawl-input", Component);
