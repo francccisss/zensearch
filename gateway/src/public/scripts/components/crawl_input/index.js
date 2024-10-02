0161fce8105531c0cd4f2e573e13784e099efa3c
francis <franzdiaz460@gmail.com>
2024-10-02 10:50:11 +0400

    ref: creating home page

---
+++ gateway/src/public/scripts/components/crawl_input/index.js
@@ -0,0 +1,20 @@
+class Component extends HTMLElement {
+  input;
+  constructor() {
+    super();
+    this.#createComponent();
+  }
+  #createComponent() {
+    const shadow = this.attachShadow({ mode: "open" });
+    const temp = document.getElementById("crawl-input-temp");
+    shadow.append(temp.content.cloneNode(true));
+    this.classList.add("crawl-input");
+  }
+}
+customElements.define("crawl-input", Component);
+
+/* Methods for Component
+ *
+ */
+
+export default { createComponent, Component };
--- gateway/src/public/scripts/utils/extract_cookies.js
+++
@@ -1,9 +0,0 @@
-export default function () {
-  let cookies = {};
-  document.cookie.split("; ").forEach((c) => {
-    const [key, value] = c.split("=");
-    cookies[`${key}`] = value;
-  });
-  console.log(cookies);
-  return cookies;
-}
