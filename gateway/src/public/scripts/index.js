57357b0417f3838d7a344b1bab9671cb742ff653
francis <franzdiaz460@gmail.com>
2024-10-02 10:50:11 +0400

    ref: creating home page

---
+++ gateway/src/public/scripts/index.js
@@ -0,0 +1,26 @@
+import crawlInput from "./components/crawl_input/index.js";
+const pageUrls = {
+  home: "/",
+  crawlProcess: "/crawl/process",
+  results: "/search/results",
+};
+
+function showPage(path) {
+  const pagesEl = document.querySelectorAll('main[id*="-page"]');
+  pagesEl.forEach((page) => {
+    page.hidden = true;
+  });
+  if (path === pageUrls.home) {
+    document.getElementById("search-page").hidden = false;
+  } else if (path === pageUrls.results) {
+    document.getElementById("results-page").hidden = false;
+  } else if (path === pageUrls.crawlProcess) {
+    document.getElementById("process-page").hidden = false;
+  }
+}
+
+function navigatePage(page) {}
+
+window.addEventListener("load", () => {
+  showPage("/");
+});
