bb473b6d1c4133c690eb3456eddd857f5c1db4ca
francis <franzdiaz460@gmail.com>
2024-10-02 23:19:45 +0400

    ref: cleaning up code for readability

--- gateway/src/public/scripts/index.js
+++ gateway/src/public/scripts/index.js
@@ -1,3 +1,4 @@
+import navigation from "./client_navigation/navigation.js";
 import crawlInput from "./components/crawl_input/index.js";
 import ui from "./ui/index.js";
 import pubsub from "./utils/pubsub.js";
@@ -6,34 +7,10 @@ const sidebar = document.getElementById("crawl-list-sb");
 const closeSbBtn = document.getElementById("close-sb");
 const openSbBtn = document.getElementById("add-entry-sb-btn");
 const listContainer = document.querySelector(".list-container");
-const pageUrls = {
-  home: "/",
-  crawlProcess: "#crawl-section/process",
-  results: "/search/results",
-};
-
-function showPage(path) {
-  const pagesEl = document.querySelectorAll('main[id*="-page"]');
-  pagesEl.forEach((page) => {
-    page.hidden = true;
-    page.style.display = "none";
-  });
-  if (path === pageUrls.home) {
-    console.log(pageUrls.home);
-    document.getElementById("search-page").hidden = false;
-    document.getElementById("search-page").style.display = "flex";
-  } else if (path === pageUrls.results) {
-    console.log(pageurls.home);
-    document.getElementById("results-page").hidden = false;
-  } else {
-    console.log(pageUrls.home);
-    document.getElementById("process-page").hidden = false;
-  }
-}
 
 window.addEventListener("load", () => {
   ui.init();
-  showPage("/");
+  navigation.showPage("/");
 });
 
 openSbBtn.addEventListener("click", () => {
