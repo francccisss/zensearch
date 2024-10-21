9b9b04fabff46ea161855e345e16d1f54f4f500f
francis <franzdiaz460@gmail.com>
2024-10-21 11:48:28 +0400

    ref: able to calculate composite queries by summing bm25ratings of each token

--- search-engine-service/bm25/idf.go
+++ search-engine-service/bm25/idf.go
@@ -1,7 +1,6 @@
 package bm25
 
 import (
-	"fmt"
 	"math"
 	"search-engine-service/utilities"
 	"strings"
@@ -9,25 +8,35 @@ import (
 
 func CalculateIDF(term string, webpages *[]utilities.WebpageTFIDF) float64 {
 
-	// totalWords := float64(wordCountInCorpa(webpages))
 	numberOfDocumentsInCorpa := float64(len(*webpages))
 	documentCountWithTerm := float64(termCountInCorpa(term, webpages))
 
-	fmt.Printf("DocCount: %f\n", numberOfDocumentsInCorpa)
-	fmt.Printf("DocsWithTerm: %f\n", documentCountWithTerm)
 	return math.Log(numberOfDocumentsInCorpa / documentCountWithTerm)
 }
 
 func termCountInCorpa(term string, webpages *[]utilities.WebpageTFIDF) int {
 	documentCount := 0
-	// corpa is just all of the webpages from different websites
+	tokenizedTerm := utilities.Tokenizer(term)
+
+	/*
+	 Using composite terms we need to check if a document
+	 contains all of these terms? or just some of the terms to
+	 constitute a document as having the tokenized term?
+	*/
+
 	for i := range *webpages {
-		// for every document in the corpa, count the documents
-		// containing the term within the document.
-		contains := strings.Contains(strings.ToLower((*webpages)[i].Contents), strings.ToLower(term))
-		if contains {
+		containsAllTerms := true
+		for j := range len(tokenizedTerm) {
+			contains := strings.Contains(strings.ToLower((*webpages)[i].Contents), strings.ToLower(tokenizedTerm[j]))
+			if !contains {
+				containsAllTerms = false
+				break
+			}
+		}
+		if containsAllTerms {
 			documentCount++
 		}
 	}
+
 	return documentCount
 }
