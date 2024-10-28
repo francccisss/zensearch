60d5e2eda94eb0674d7a49e1ab5f64aa40661e9a
francis <franzdiaz460@gmail.com>
2024-10-29 00:04:24 +0400

    fix: node:alpine as base image causes sqlite3 to crash, using node version 20.16.0 works as it is the current version for development

--- database-service/src/index.ts
+++ database-service/src/index.ts
@@ -3,13 +3,23 @@ import path from "path";
 import amqp from "amqplib";
 import database_operations from "./database_operations";
 import channel_operations from "./rabbitmq/channel_operations";
+import { readFile } from "fs";
 
 const db = init_database();
-(async () => {
-  const connection = await amqp.connect("amqp://rabbitmq:5672");
+//exec_scripts(db, path.join(__dirname, "./db_utils/websites.init.sql"));
 
-  const database_channel = await connection.createChannel();
-  await channel_operations.channel_handler(db, database_channel);
+(async () => {
+  const connection = await amqp.connect("amqp://rabbitmq");
+  console.log("Connected to rabbitmq");
+  try {
+    const database_channel = await connection.createChannel();
+    console.log("Channel Created");
+    await channel_operations.channel_handler(db, database_channel);
+  } catch (err) {
+    const error = err as Error;
+    console.error(error.message);
+    console.log("ERROR: Unable to create a channel for databas service.");
+  }
 })();
 
 function init_database(): sqlite3.Database {
@@ -24,8 +34,27 @@ function init_database(): sqlite3.Database {
         console.error("Unable to connect to website_collection.db");
         process.exit(1);
       }
-      console.log("Thread connected to database");
+      console.log("Connected to sqlite3 database.");
     },
   );
   return db;
+}
+
+async function exec_scripts(db: sqlite3.Database, scriptPath: string) {
+  console.log("Execute sqlite script");
+  readFile(scriptPath, "utf-8", (err, data) => {
+    const stmts = data
+      .split(";")
+      .map((stmt) => stmt.trim())
+      .filter((stmt) => stmt);
+
+    stmts.forEach((statement) => {
+      db.run(statement, [], (err) => {
+        if (err) {
+          console.error(err);
+        }
+        console.log("Executed statement: %s ", statement);
+      });
+    });
+  });
 }
