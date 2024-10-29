1516d729095562850d78fc930e4c59e63e2de6db
francis <franzdiaz460@gmail.com>
2024-10-29 11:56:10 +0400

    doc:updated script and readme instructions

--- build-dependencies.sh
+++ build-dependencies.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
-steps=('./database-service' './gateway/' './web-crawler-service/')
+steps=('./database-service' './gateway/' )
 for i in "${steps[@]}"; do
   cd "$i"
   echo "Processing"
   echo "$i"
   npm i
+  npm run build
   cd ../
   echo "Done."
 done
-echo "Done"
+
+
+steps=('./web-crawler-service/' './search-engine-service/' )
+for i in "${steps[@]}"; do
+  cd "$i"
+  echo "Processing"
+  echo "$i"
+  go mod tidy
+  go mod build
+  cd ../
+  echo "Done."
+done
