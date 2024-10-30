497e66b3d2d9ef157589fd3cd47638eb29250d86
francis <franzdiaz460@gmail.com>
2024-10-30 19:21:37 +0400

    ref: adding logs to better track the errors

--- web-crawler-service/crawler_test.go
+++ web-crawler-service/crawler_test.go
@@ -2,20 +2,11 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
 	"testing"
-	"web-crawler-service/pkg/webdriver"
 )
 
 func TestTraversal(t *testing.T) {
-
-	service, err := webdriver.CreateWebDriverServer()
-	defer service.Stop()
-	if err != nil {
-		log.Print("INFO: Retry web driver server or the application.\n")
-		log.Print(err.Error())
-	}
 
 	Docs := os.Args[3:]
 	fmt.Printf("\nTest Argument : %+v\n", os.Args[3:][0])
