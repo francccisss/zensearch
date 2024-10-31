84fc3fbe2ffb0105b291eaf07cf1045603d96180
francis <franzdiaz460@gmail.com>
2024-10-31 11:11:32 +0400

    ref: updating compose for selenium container used by crawler

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
+docker compose down && \
 docker ps -a | grep "zensearch" | awk '{print $1}' | xargs docker stop \
 && docker ps -a | grep "zensearch" | awk '{print $1}' | xargs docker rm \
 && docker images | grep "zensearch" | awk '{print $1}' | xargs docker rmi \
-
-
+&& docker compose up
