c278c1227dcaeebe712c63574204307fd359c79e
francis <franzdiaz460@gmail.com>
2024-10-31 10:42:01 +0400

    ref: using javascript to grab all of achor tags and asserting interface to type []string conversion

--- web-crawler-service/crawler.go
+++ web-crawler-service/crawler.go
@@ -65,8 +65,6 @@ const (
 	threadPool   = 10
 	crawlFail    = 0
 	crawlSuccess = 1
-	// removes links to web objects that does not return an html page.
-	linkFilter = `a:not([href$=".zip"]):not([href$=".svg"]):not([href$=".scss"]):not([href$=".css"]):not([href$=".pdf"]):not([href$=".exe"]):not([href$=".jpg"]):not([href$=".png"]):not([href$=".tar.gz"]):not([href$=".rar"]):not([href$=".7z"]):not([href$=".mp3"]):not([href$=".mp4"]):not([href$=".mkv"]):not([href$=".tar"]):not([href$=".xz"]):not([href$=".msi"])`
 )
 
 type PageResult struct {
--- web-crawler-service/utilities/origin_url.go
+++ web-crawler-service/utilities/origin_url.go
@@ -1,14 +1,13 @@
 package utilities
 
 import (
-	"fmt"
 	"net/url"
 )
 
 func GetHostname(currentUrl string) (string, string, error) {
 	u, err := url.Parse(currentUrl)
 	if err != nil {
-		return "", "", fmt.Errorf(err.Error())
+		return "", "", err
 	}
 	return u.Hostname(), u.Path, nil
 }
