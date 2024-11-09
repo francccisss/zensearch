0d68e5e98ad960fd8990a8d338d8642d372186d0
francis <franzdiaz460@gmail.com>
2024-11-09 21:07:29 +0400

    ref: is this concurrency pipeline?

--- search-engine/main.go
+++ search-engine/main.go
@@ -51,9 +51,6 @@ func main() {
 	failOnError(err, "Failed to create db response queue")
 	// DECLARING QUEUES
 
-	if err != nil {
-		fmt.Printf("Unable to listen to %s", rabbitmq.DB_RESPONSE_QUEUE)
-	}
 	msgs, err := mainChannel.Consume(
 		rabbitmq.SEARCH_QUEUE,
 		"",
@@ -70,55 +67,66 @@ func main() {
 	searchQueryChan := make(chan string)
 
 	// Concurrency Pipline
-	for {
-		select {
-		case userSearch := <-msgs:
-			{
-				searchQuery := string(userSearch.Body)
-				if searchQuery == "" {
-					log.Panicf("Search Query is empty\n")
-				}
-				rabbitmq.QueryDatabase(searchQuery)
-				mainChannel.Ack(userSearch.DeliveryTag, true)
+	go func(searchQueryChan <-chan string) {
+		for {
+			searchQuery := <-searchQueryChan
 
-				// block until we received a search query
-				searchQueryChan <- searchQuery
-				fmt.Print("Process Done.\n")
-				fmt.Printf("User's Query: %s\n", searchQuery)
+			// Should i use go routines? its still going to be an unbuffered channel anyways
+			// so might as well just make everything synchronous
+
+			fmt.Println("Search query retrieved: `%s`", searchQuery)
+
+			webpageBytes, err := ListenIncomingSegments(searchQuery)
+			fmt.Printf("Total Byte Length: %d\n", len(webpageBytes))
+
+			if err != nil {
+				fmt.Printf("Something went wrong while listening to incoming data segments from database\n")
+				log.Printf(err.Error())
+			} // DONT TOUCH THIS
+			webpages, err := ParseWebpages(webpageBytes)
+
+			if err != nil {
+				fmt.Printf(err.Error())
+				log.Panicf("Unable to parse webpages")
 			}
-		case searchQuery := <-searchQueryChan:
-			{
-				// Should i use go routines? its still going to be an unbuffered channel anyways
-				// so might as well just make everything synchronous
+			// create segments in this section after ranking
+			calculatedRatings := bm25.CalculateBMRatings(searchQuery, webpages, bm25.AvgDocLen(webpages))
+			rankedWebpages := bm25.RankBM25Ratings(calculatedRatings)
+			fmt.Printf("Search Query for composite query: %s\n\n", searchQuery)
 
-				webpageBytes, err := ListenIncomingSegments(searchQuery)
-
-				if err != nil {
-					fmt.Printf("Something went wrong while listening to incoming data segments from database\n")
-					log.Printf(err.Error())
-				}
-
-				// DONT TOUCH THIS
-				webpages, err := ParseWebpages(webpageBytes)
-				if err != nil {
-					fmt.Printf(err.Error())
-					log.Panicf("Unable to parse webpages")
-				}
-				// create segments in this section after ranking
-				calculatedRatings := bm25.CalculateBMRatings(searchQuery, webpages, bm25.AvgDocLen(webpages))
-				rankedWebpages := bm25.RankBM25Ratings(calculatedRatings)
-				fmt.Printf("Search Query for composite query: %s\n\n", searchQuery)
-
-				_, err = CreateSegments(rankedWebpages, MSS)
-				if err != nil {
-					fmt.Println(err.Error())
-					log.Println("Unable to create segments")
-				}
-
-				rabbitmq.PublishScoreRanking(rankedWebpages)
+			_, err = CreateSegments(rankedWebpages, MSS)
+			if err != nil {
+				fmt.Println(err.Error())
+				log.Panicf("Unable to create segments")
 			}
+			// rabbitmq.PublishScoreRanking(rankedWebpages)
 		}
+	}(searchQueryChan)
+
+	go func(searchQueryChan chan string) {
+		for {
+			userSearch := <-msgs
+			searchQuery := string(userSearch.Body)
+			if searchQuery == "" {
+				log.Panicf("Search Query is empty\n")
+			}
+
+			fmt.Printf("User's Query: %s\n", searchQuery)
+			mainChannel.Ack(userSearch.DeliveryTag, true)
+			// block until we received a search query
+			// if Process Done log is not called means
+			// searchQueryChan is not working for some reason
+			searchQueryChan <- searchQuery
+			fmt.Print("Process Done.\n")
+			rabbitmq.QueryDatabase(searchQuery)
+		}
+	}(searchQueryChan)
+
+	// need to signal this loop to stop if error or graceful exits
+	for {
+
 	}
+
 }
 
 // maybe use message for cache validation later on for optimization
@@ -133,7 +141,7 @@ func ParseWebpages(data []byte) (*[]utilities.WebpageTFIDF, error) {
 	var webpages []utilities.WebpageTFIDF
 	err := json.Unmarshal(data, &webpages)
 	if err != nil {
-		return &[]utilities.WebpageTFIDF{}, err
+		return nil, err
 	}
 	return &webpages, nil
 }
