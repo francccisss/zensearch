eb049629811e76b0616a487c3bc10461fb207989
francis <franzdiaz460@gmail.com>
2025-02-28 18:00:46 +0400

    ref: containing container creation

--- command_test.go
+++ command_test.go
@@ -40,26 +40,29 @@ func stringToArr(str string) []string {
 // for rabbitmq and selenium containers
 
 type ClientContainer struct {
-	Client         *client.Client
-	HostPorts      [][]string
-	ContainerPorts [][]string
-	ContainerName  string
-	ContainerID    string
+	Client *client.Client
+	HostPorts
+	ContainerPorts
+	ContainerName string
+	ContainerID   string
 }
+
+type HostPorts [][]string
+type ContainerPorts [][]string
 
 type Container interface {
 	Run()
 	Start()
 	// Restart()
-	BuildImage()
+	Create(ctx context.Context)
 	Stop()
 }
 
 // Returnes specific container using filter to isolate container name
 // used for checking duplicate containers
-func (cc ClientContainer) getContainer(name string, ctx context.Context) (container.Summary, error) {
+func (cc ClientContainer) getContainer(ctx context.Context) (container.Summary, error) {
 	filter := filters.NewArgs()
-	filter.Add("name", name)
+	filter.Add("name", cc.ContainerName)
 
 	containers, err := cc.Client.ContainerList(ctx, container.ListOptions{Size: false, Filters: filter, All: true})
 	if err != nil {
@@ -68,7 +71,7 @@ func (cc ClientContainer) getContainer(name string, ctx context.Context) (contai
 	}
 
 	if len(containers) == 0 {
-		return container.Summary{}, fmt.Errorf("container %s does not exist", name)
+		return container.Summary{}, fmt.Errorf("container %s does not exist", cc.ContainerName)
 	}
 	return containers[0], nil
 
@@ -88,6 +91,40 @@ func (cc *ClientContainer) Run(ctx context.Context, imageName string, tag string
 	io.Copy(os.Stdout, reader)
 	defer reader.Close()
 
+	err = cc.Create(ctx, imageName, tag)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Docker: starting %s container...\n", cc.ContainerName)
+
+	if err := cc.Client.ContainerStart(ctx, cc.ContainerID, container.StartOptions{}); err != nil {
+		fmt.Println("Unable to start rabbitmq container")
+		panic(err)
+	}
+	// dont know when it is completely finished, need to set a timer for other
+	// process that depends on rabbitmq
+	fmt.Printf("Docker: %s container started!\n", cc.ContainerName)
+	fmt.Printf("Docker: %s container exposed ports -> %s, %s\n", cc.ContainerName, ":5672", ":15672")
+
+	fmt.Printf("Docker: waiting for %s container status...\n", cc.ContainerName)
+	statusCh, errCh := cc.Client.ContainerWait(ctx, cc.ContainerID, container.WaitConditionNotRunning)
+	select {
+	case err := <-errCh:
+		fmt.Println("Container state not positive")
+		panic(err)
+	case s := <-statusCh:
+		fmt.Println("Container status:")
+		if s.Error == nil {
+			fmt.Println("Docker: container closed gracefully")
+		}
+	}
+	out, _ := cc.Client.ContainerLogs(ctx, cc.ContainerID, container.LogsOptions{ShowStdout: true, ShowStderr: true})
+	stdcopy.StdCopy(os.Stdout, os.Stderr, out)
+}
+
+func (cc *ClientContainer) Create(ctx context.Context, imageName string, tag string) error {
+
+	imageNameWithTag := imageName + ":" + tag
 	hostPorts := map[nat.Port][]nat.PortBinding{}
 	for _, hostPort := range cc.HostPorts {
 		_, ok := hostPorts[nat.Port(fmt.Sprintf("%s/tcp", hostPort[0]))]
@@ -96,7 +133,7 @@ func (cc *ClientContainer) Run(ctx context.Context, imageName string, tag string
 			hostPorts[nat.Port(fmt.Sprintf("%s/tcp", hostPort[0]))] = append(contPorts, nat.PortBinding{HostIP: "0.0.0.0", HostPort: hostPort[1]})
 		}
 	}
-	containerPorts := map[nat.Port]struct{}{nat.Port("5672/tcp"): {}, nat.Port("15672/tcp"): {}}
+	containerPorts := map[nat.Port]struct{}{}
 	for _, contPort := range cc.ContainerPorts {
 		_, ok := hostPorts[nat.Port(fmt.Sprintf("%s/tcp", contPort[0]))]
 		if ok {
@@ -120,34 +157,13 @@ func (cc *ClientContainer) Run(ctx context.Context, imageName string, tag string
 			},
 			PortBindings: hostPorts}, nil, nil, cc.ContainerName)
 
+	if err != nil {
+		fmt.Printf("Docker: ERROR was not able to create %s container", cc.ContainerName)
+		panic(err)
+	}
 	fmt.Printf("Docker: %s's container ID %s\n", cc.ContainerName, resp.ID)
 	cc.ContainerID = resp.ID
-
-	fmt.Printf("Docker: starting %s container...\n", cc.ContainerName)
-	if err := cc.Client.ContainerStart(ctx, cc.ContainerID, container.StartOptions{}); err != nil {
-		fmt.Println("Unable to start rabbitmq container")
-		panic(err)
-	}
-
-	// dont know when it is completely finished, need to set a timer for other
-	// process that depends on rabbitmq
-	fmt.Printf("Docker: %s container started!\n", cc.ContainerName)
-	fmt.Printf("Docker: %s container exposed ports -> %s, %s\n", cc.ContainerName, ":5672", ":15672")
-
-	fmt.Printf("Docker: waiting for %s container status...\n", cc.ContainerName)
-	statusCh, errCh := cc.Client.ContainerWait(ctx, cc.ContainerID, container.WaitConditionNotRunning)
-	select {
-	case err := <-errCh:
-		fmt.Println("Container state not positive")
-		panic(err)
-	case s := <-statusCh:
-		fmt.Println("Container status:")
-		if s.Error == nil {
-			fmt.Println("Docker: container closed gracefully")
-		}
-	}
-	out, _ := cc.Client.ContainerLogs(ctx, cc.ContainerID, container.LogsOptions{ShowStdout: true, ShowStderr: true})
-	stdcopy.StdCopy(os.Stdout, os.Stderr, out)
+	return nil
 }
 
 func TestDockerRabbitmq(t *testing.T) {
@@ -158,89 +174,19 @@ func TestDockerRabbitmq(t *testing.T) {
 	if err != nil {
 		log.Panic(err.Error())
 	}
+	clientContainer := ClientContainer{
+		Client:         cli,
+		ContainerName:  "zensearch-cli-rabbitmq",
+		HostPorts:      HostPorts{{"5672", "5672"}, {"15672", "15672"}},
+		ContainerPorts: ContainerPorts{{"5672", "5672"}, {"15672", "15672"}}}
 	defer cli.Close()
 
-	fmt.Printf("Docker: connection successful\n")
-
-	var currentContainerID string
-	imageName := "rabbitmq"
-	imageNameWithTag := imageName + ":4.0-management"
-
-	setContainerName := "zensearch-rabbitmq-cli"
-	filter := filters.NewArgs()
-	filter.Add("name", setContainerName)
-
-	fmt.Printf("Docker: starting %s container...\n", setContainerName)
-
-	containers, err := cli.ContainerList(ctx, container.ListOptions{Size: false, Filters: filter, All: true})
+	_, err = clientContainer.getContainer(ctx)
 	if err != nil {
-		fmt.Println("Unable to get list of containers")
-		panic(err)
+		fmt.Printf("Docker: %s", err.Error())
+		clientContainer.Create(ctx, "rabbitmq", "4.0-management")
+
 	}
-
-	if len(containers) == 0 {
-
-		fmt.Printf("Docker: container does not exist creating %s container\n", setContainerName)
-		fmt.Printf("Docker: pulling %s image...\n", imageNameWithTag)
-		reader, err := cli.ImagePull(ctx, imageNameWithTag, image.PullOptions{})
-		if err != nil {
-			log.Panic(err)
-		}
-
-		io.Copy(os.Stdout, reader)
-		defer reader.Close()
-
-		mappedPort := nat.PortMap{nat.Port("5672/tcp"): {nat.PortBinding{HostIP: "0.0.0.0", HostPort: "5672"}}, nat.Port("15672/tcp"): {nat.PortBinding{HostIP: "0.0.0.0", HostPort: "15672"}}}
-		containerPorts := nat.PortSet{nat.Port("5672/tcp"): struct{}{}, nat.Port("15672/tcp"): struct{}{}}
-		// grabs latest version of rabbitmq
-		fmt.Printf("Docker: creating container from %s image as %s \n", imageNameWithTag, setContainerName)
-		resp, err := cli.ContainerCreate(ctx, &container.Config{
-			Image: imageNameWithTag,
-			// attaching container to process exec is on `-it`
-			AttachStdin:  true,
-			AttachStdout: true,
-			AttachStderr: true,
-			ExposedPorts: containerPorts,
-		},
-			&container.HostConfig{
-				Binds: []string{
-					"/var/run/docker.sock:/var/run/docker.sock",
-				},
-				PortBindings: mappedPort}, nil, nil, setContainerName)
-
-		fmt.Printf("Docker: %s's container ID %s\n", setContainerName, resp.ID)
-		currentContainerID = resp.ID
-
-	} else {
-		currentContainerID = containers[0].ID
-		fmt.Printf("Docker: container already exists from %s image as %s \n", imageNameWithTag, containers[0].Names)
-		fmt.Printf("Docker: %s's container ID %s\n", setContainerName, currentContainerID)
-	}
-	fmt.Printf("Docker: starting %s container...\n", setContainerName)
-	if err := cli.ContainerStart(ctx, currentContainerID, container.StartOptions{}); err != nil {
-		fmt.Println("Unable to start rabbitmq container")
-		panic(err)
-	}
-
-	// dont know when it is completely finished, need to set a timer for other
-	// process that depends on rabbitmq
-	fmt.Printf("Docker: %s container started!\n", setContainerName)
-	fmt.Printf("Docker: %s container exposed ports -> %s, %s\n", setContainerName, ":5672", ":15672")
-
-	fmt.Printf("Docker: waiting for %s container status...\n", setContainerName)
-	statusCh, errCh := cli.ContainerWait(ctx, currentContainerID, container.WaitConditionNotRunning)
-	select {
-	case err := <-errCh:
-		fmt.Println("Container state not positive")
-		panic(err)
-	case s := <-statusCh:
-		fmt.Println("Container status:")
-		if s.Error == nil {
-			fmt.Println("Docker: container closed gracefully")
-		}
-	}
-	out, _ := cli.ContainerLogs(ctx, currentContainerID, container.LogsOptions{ShowStdout: true, ShowStderr: true})
-	stdcopy.StdCopy(os.Stdout, os.Stderr, out)
 
 }
 
