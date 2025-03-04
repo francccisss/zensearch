9ddf82100a84fcc934f89b01134b2d372f6e0b86
francis <franzdiaz460@gmail.com>
2025-03-04 17:19:22 +0400

    ref: named commands for database abd express was swapped

--- cli_test.go
+++ cli_test.go
@@ -1,20 +1,12 @@
 package main
 
 import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
 	"testing"
 )
-
-var buildCmds = [][]string{
-	{"express", "npm", "run", "build", "--prefix", "./database"},
-	{"database", "npm", "run", "build", "--prefix", "./express-server"},
-	{"crawler", "go", "build", "-C", "./crawler/"},
-	{"search-engine", "go", "build", "-C", "./search-engine/"},
-}
-
-var npmInstall = [][]string{
-	{"express", "npm", "install", "database/"},
-	{"database", "npm", "install", "express-server/"},
-}
 
 func TestNpmCmd(t *testing.T) {
 	runCommands(npmInstall, &errArr)
@@ -25,4 +17,22 @@ func TestBuildCmd(t *testing.T) {
 
 func TestRunCmd(t *testing.T) {
 	startServices(runCmds)
+}
+
+func TestSignalINT(t *testing.T) {
+	sigs := make(chan os.Signal, 1)
+
+	signal.Notify(sigs, syscall.SIGINT)
+	done := make(chan bool, 1)
+
+	go func() {
+		sig := <-sigs
+		fmt.Println()
+		fmt.Println(sig)
+		done <- true
+	}()
+
+	fmt.Println("awaiting signal")
+	<-done
+	fmt.Println("exiting")
 }
