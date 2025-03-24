21694609d2666464c8493e3c14dc0194636c5310
francis-lp-win <franzdiaz460@gmail.com>
2025-03-24 17:13:53 +0400

    ref: switching to better-sqlite3

--- database/src/database.ts
+++ database/src/database.ts
@@ -1,200 +1,189 @@
-import { Database } from "sqlite3";
 import { IndexedWebpages, URLs, Webpage, FrontierQueue } from "./utils/types";
 import { randomUUID } from "crypto";
+import Database from "better-sqlite3";
 
-async function indexWebpages(db: Database, data: IndexedWebpages) {
-  if (db == null) {
-    throw new Error("ERROR: Database is not connected.");
-  }
-  db.serialize(() => {
-    db.run(
-      "INSERT OR IGNORE INTO known_sites (url, last_added) VALUES ($url, $last_added);",
-      {
-        $url: data.URLSeed,
-        $last_added: Date.now(),
-      },
-    );
-    const insertIndexedSitesStmt = db.prepare(
-      "INSERT OR IGNORE INTO indexed_sites (primary_url, last_indexed) VALUES ($primary_url, $last_indexed);",
-    );
-    const insertWebpagesStmt = db.prepare(
-      "INSERT INTO webpages (url, title, contents, parent) VALUES ($webpage_url, $title, $contents, $parent);",
-    );
-    insertIndexedSitesStmt.run(
-      {
-        $primary_url: data.URLSeed,
-        $last_indexed: Date.now(),
-      },
-      function (err) {
-        if (err) {
-          console.error("Unable to add last indexed site:", err.message);
-          return;
-        }
-        const parentId = this.lastID;
-        data.Webpages.forEach((el) => {
-          if (el === undefined) return;
-          const {
-            Header: { Title, Url },
-            Contents,
-          } = el;
-
-          insertWebpagesStmt.run(
-            {
-              $webpage_url: Url,
-              $title: Title,
-              $contents: Contents,
-              $parent: parentId,
-            },
-            (err) => {
-              if (err) {
-                console.error("Error inserting webpage:", err.message);
-              }
-            },
-          );
-        });
-        insertWebpagesStmt.finalize();
-      },
-    );
-    insertIndexedSitesStmt.finalize();
-  });
-  console.log("NOTIF: DONE INDEXING");
+async function indexWebpages(db: Database.Database, data: IndexedWebpages) {
+  //if (db == null) {
+  //  throw new Error("ERROR: Database is not connected.");
+  //}
+  //db.serialize(() => {
+  //  db.run(
+  //    "INSERT OR IGNORE INTO known_sites (url, last_added) VALUES ($url, $last_added);",
+  //    {
+  //      $url: data.URLSeed,
+  //      $last_added: Date.now(),
+  //    },
+  //  );
+  //  const insertIndexedSitesStmt = db.prepare(
+  //    "INSERT OR IGNORE INTO indexed_sites (primary_url, last_indexed) VALUES ($primary_url, $last_indexed);",
+  //  );
+  //  const insertWebpagesStmt = db.prepare(
+  //    "INSERT INTO webpages (url, title, contents, parent) VALUES ($webpage_url, $title, $contents, $parent);",
+  //  );
+  //  insertIndexedSitesStmt.run(
+  //    {
+  //      $primary_url: data.URLSeed,
+  //      $last_indexed: Date.now(),
+  //    },
+  //    function (err) {
+  //      if (err) {
+  //        console.error("Unable to add last indexed site:", err.message);
+  //        return;
+  //      }
+  //      const parentId = this.lastID;
+  //      data.Webpages.forEach((el) => {
+  //        if (el === undefined) return;
+  //        const {
+  //          Header: { Title, Url },
+  //          Contents,
+  //        } = el;
+  //
+  //        insertWebpagesStmt.run(
+  //          {
+  //            $webpage_url: Url,
+  //            $title: Title,
+  //            $contents: Contents,
+  //            $parent: parentId,
+  //          },
+  //          (err) => {
+  //            if (err) {
+  //              console.error("Error inserting webpage:", err.message);
+  //            }
+  //          },
+  //        );
+  //      });
+  //      insertWebpagesStmt.finalize();
+  //    },
+  //  );
+  //  insertIndexedSitesStmt.finalize();
+  //});
+  //console.log("NOTIF: DONE INDEXING");
 }
 
-async function queryWebpages(db: Database): Promise<Array<Webpage>> {
+async function queryWebpages(db: Database.Database): Promise<Array<Webpage>> {
   // query function returns once the promise has either been resolved
   // or rejected by the sqlite query call.
 
   return await new Promise(function (resolved, reject) {
-    const sql_query = "SELECT Url, Contents, Title FROM webpages";
-    db.all(sql_query, (err: Error, rows: Array<Webpage>) => {
-      try {
-        if (err) {
-          throw new Error(
-            "ERROR: Something went wrong while querying webpages for search query.",
-          );
-        }
-        if (rows.length === 0) {
-          console.log("There are 0 webpages.\n");
-          console.log("Please Crawl the web. \n");
-        }
-        resolved(rows);
-      } catch (err) {
-        const error = err as Error;
-        console.log(
-          "ERROR: Error while querying webpages in database service.",
-        );
-        console.error(error.message);
-        reject(error.message);
-      }
-    });
+    //  const sql_query = "SELECT Url, Contents, Title FROM webpages";
+    //  db.all(sql_query, (err: Error, rows: Array<Webpage>) => {
+    //    try {
+    //      if (err) {
+    //        throw new Error(
+    //          "ERROR: Something went wrong while querying webpages for search query.",
+    //        );
+    //      }
+    //      if (rows.length === 0) {
+    //        console.log("There are 0 webpages.\n");
+    //        console.log("Please Crawl the web. \n");
+    //      }
+    //      resolved(rows);
+    //    } catch (err) {
+    //      const error = err as Error;
+    //      console.log(
+    //        "ERROR: Error while querying webpages in database service.",
+    //      );
+    //      console.error(error.message);
+    //      reject(error.message);
+    //    }
+    //  });
   });
 }
 
 // TODO clean this wrapper thingy majig next time. :D
 async function checkExistingTasks(
-  db: Database,
+  db: Database.Database,
   crawlList: Array<string>,
 ): Promise<Array<string>> {
   let tmp: Array<string> = [];
-  const query = await queryPromiseWrapper(db);
-  if (query == null)
-    throw new Error("ERROR: Unable to query indexed websites.");
-  crawlList.forEach((item) => {
-    const url = new URL(item).hostname;
-    if (!query.has(url)) {
-      tmp.push(item);
-    }
-  });
+  //const query = await queryPromiseWrapper(db);
+  //if (query == null)
+  //  throw new Error("ERROR: Unable to query indexed websites.");
+  //crawlList.forEach((item) => {
+  //  const url = new URL(item).hostname;
+  //  if (!query.has(url)) {
+  //    tmp.push(item);
+  //  }
+  //});
   return tmp;
 }
 
 async function queryPromiseWrapper(
-  db: Database,
+  db: Database.Database,
 ): Promise<Map<string, string> | null> {
   const stmt = `SELECT primary_url FROM indexed_sites`;
   let indexed_map: Map<string, string> = new Map();
   return new Promise((resolve, reject) => {
-    db.each(
-      stmt,
-      function (err: Error, row: { primary_url: string }) {
-        try {
-          if (err) {
-            throw new Error(err.message);
-          }
-          indexed_map.set(row.primary_url, "");
-        } catch (err) {
-          console.error("ERROR: Unable to query indexed websites.");
-          console.error(err);
-          reject(null);
-        }
-      },
-      function (err) {
-        if (err) {
-          console.error(err);
-          reject(null);
-          return;
-        }
-        resolve(indexed_map);
-      },
-    );
+    //db.each(
+    //  stmt,
+    //  function (err: Error, row: { primary_url: string }) {
+    //    try {
+    //      if (err) {
+    //        throw new Error(err.message);
+    //      }
+    //      indexed_map.set(row.primary_url, "");
+    //    } catch (err) {
+    //      console.error("ERROR: Unable to query indexed websites.");
+    //      console.error(err);
+    //      reject(null);
+    //    }
+    //  },
+    //  function (err) {
+    //    if (err) {
+    //      console.error(err);
+    //      reject(null);
+    //      return;
+    //    }
+    //    resolve(indexed_map);
+    //  },
+    //);
   });
 }
 
 const testDb: Map<string, Array<string>> = new Map();
-async function storeURLs(db: Database, Urls: URLs) {
-  db.serialize(() => {
-    db.get(
-      "SELECT Domain FROM Queue WHERE Domain = $Domain;",
-      {
-        $Domain: Urls.Domain,
-      },
-      (err: Error, row: Array<FrontierQueue> | FrontierQueue) => {
-        if (err != null) {
-          console.error(err);
-          return;
-        }
-
-        if (row == undefined) {
-          db.run("INSERT INTO Queue (ID,Domain) VALUES ($ID, $Domain);", {
-            $ID: randomUUID(),
-            $Domain: Urls.Domain,
-          });
-        }
-      },
-    );
-
-    db.get(
-      "SELECT * FROM Queue WHERE Domain = $Domain;",
-      {
-        $Domain: Urls.Domain,
-      },
-      (err: Error, row: Array<FrontierQueue> | FrontierQueue) => {
-        if (err != null) {
-          console.error(err);
-          return;
-        }
-        console.log(row);
-      },
-    );
-  });
-
-  if (!testDb.has(Urls.Domain)) {
-    console.log("Creating queue for %s", Urls.Domain);
-    console.log("Stored URLS in FrontierQueue for %s", Urls.Domain);
-    testDb.set(Urls.Domain, Urls.Nodes);
-    return;
-  }
-  testDb.set(Urls.Domain, Urls.Nodes);
-  console.log("Stored URLS in FrontierQueue for %s", Urls.Domain);
+async function storeURLs(db: Database.Database, Urls: URLs) {
+  //db.serialize(() => {
+  //  db.get(
+  //    "SELECT Domain FROM Queue WHERE Domain = $Domain;",
+  //    { $Domain: Urls.Domain },
+  //    (err: Error, row: Array<FrontierQueue> | FrontierQueue) => {
+  //      if (err != null) {
+  //        console.error(err);
+  //        throw new Error(err.message);
+  //      }
+  //      if (row == undefined) {
+  //        db.run("INSERT INTO Queue (ID,Domain) VALUES ($ID, $Domain);", {
+  //          $ID: randomUUID(),
+  //          $Domain: Urls.Domain,
+  //        });
+  //      }
+  //      Urls.Nodes.forEach((url) => {
+  //        db.run(
+  //          "INSERT INTO Node (ID, Url, QueueID) VALUES ($ID, $Url, $QueueID);",
+  //          { $ID: randomUUID(), $Url: url, $QueueID: "21" },
+  //        );
+  //      });
+  //    },
+  //  );
+  //});
+  //
+  //if (!testDb.has(Urls.Domain)) {
+  //  console.log("Creating queue for %s", Urls.Domain);
+  //  console.log("Stored URLS in FrontierQueue for %s", Urls.Domain);
+  //  testDb.set(Urls.Domain, Urls.Nodes);
+  //  return;
+  //}
+  //testDb.set(Urls.Domain, Urls.Nodes);
+  //console.log("Stored URLS in FrontierQueue for %s", Urls.Domain);
 }
 
-async function clearURLs(db: Database, q: FrontierQueue) {
+async function clearURLs(db: Database.Database, q: FrontierQueue) {
   console.log(q);
   console.log("Storing URLS in FrontierQueue");
 }
 
 async function dequeueURL(
-  db: Database,
+  db: Database.Database,
   src: string,
 ): Promise<{ length: number; url: string }> {
   return { length: 0, url: "fzaid.vercel.app/home" };
--- database/src/index.ts
+++ database/src/index.ts
@@ -1,7 +1,8 @@
 import path from "path";
-import sqlite3 from "sqlite3";
+import Database from "better-sqlite3";
 import rabbitmq from "./rabbitmq";
 import { readFile } from "fs";
+import { exit } from "node:process";
 
 const wc = "../website_collection.db";
 const fq = "../frontier_queue.db";
@@ -22,6 +23,11 @@ exec_scripts(
     const frontierChannel = await connection.createChannel();
     console.log("Channel Created");
     databaseChannel.prefetch(cumulativeAckCount, false);
+    if (websitesDB == null || frontierQueueDB == null) {
+      // TODO need to tell users which database initialization failed
+      console.error("ERROR: unable to initialize databases");
+      exit(1);
+    }
     rabbitmq.webpageHandler(websitesDB, databaseChannel);
     rabbitmq.frontierQueueHandler(frontierQueueDB, frontierChannel);
   } catch (err) {
@@ -30,38 +36,31 @@ exec_scripts(
   }
 })();
 
-function init_database(src: string): sqlite3.Database {
-  const sqlite = sqlite3.verbose();
-  const db = new sqlite.Database(
-    path.join(__dirname, src),
-    sqlite.OPEN_READWRITE,
-    (err: Error | null) => {
-      if (err != null) {
-        console.error(err);
-        console.error(err.message);
-        console.error(
-          "Unable to connect to website_collection.db make sure it exists first",
-        );
-        process.exit(1);
-      }
-      console.log("Connected to sqlite3 database.");
-    },
-  );
-  return db;
+function init_database(src: string): Database.Database | null {
+  try {
+    const db = new Database(src);
+    return db;
+  } catch (err: any) {
+    console.error("ERROR: Unable to initialize database %s", err.message);
+    console.error(err);
+    return null;
+  }
 }
 
-async function exec_scripts(db: sqlite3.Database, scriptPath: string) {
+async function exec_scripts(db: Database.Database | null, scriptPath: string) {
   console.log("Execute sqlite script");
+  console.log(scriptPath);
+  if (db === null) {
+    console.error("ERROR: database does not exist for %s", scriptPath);
+    exit(1);
+  }
   readFile(scriptPath, "utf-8", (_, data) => {
     const stmts = data
       .split(";")
       .map((stmt) => stmt.trim())
       .filter((stmt) => stmt);
-
-    stmts.forEach((statement) => {
-      db.run(statement, [], (_) => {
-        console.log("Executed statement: %s ", statement);
-      });
+    stmts.forEach((stmt) => {
+      db?.exec(stmt);
     });
   });
 }
