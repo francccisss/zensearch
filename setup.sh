8b7184b28df0e2f853c453cc89f9edca57bd72a0
francis <franzdiaz460@protonmail.com>
2026-06-18 13:14:29 +0400

    ref: initializing database table from script instead from service

--- setup.sh
+++ setup.sh
@@ -11,6 +11,8 @@ echo -e "Welcome to zensearch setup.\n"
 echo -e "This Setup will need to access your database engine via root for creating
 your zensearch database and user.\n"
 
+
+
 read -p "Zensearch Database Name: " DB_NAME
 
 read -p "Zensearch Database User: " DB_USER
@@ -20,7 +22,7 @@ read -p "Zensearch Database Host: " DB_HOST
 read -sp "Zensearch Database Pass: " DB_PASS
 
 
-echo -e "\nCreating $DB_NAME Database and $DB_USER User."
+echo -e "\nCreating $DB_NAME Database and $DB_USER User.\n"
 
 sudo mariadb << EOF
 CREATE DATABASE IF NOT EXISTS $DB_NAME;
@@ -28,12 +30,29 @@ CREATE USER IF NOT EXISTS '$DB_USER'@'$DB_HOST' IDENTIFIED BY '$DB_PASS';
 GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'$DB_HOST';
 FLUSH PRIVILEGES;
 EOF
+echo -e "--------------------------------------------"
+echo -e "Checking if Database: $DB_NAME was created.\nEnter your password for $DB_USER in $DB_NAME\n"
+
+DATABASE_LIST=$(echo "show databases;" | sudo mariadb -u root -p) 
+
+for DB in $DATABASE_LIST 
+do
+	if  [ "zensearch_db"="$DB" ]; then
+		echo "Database is created moving on."
+		break
+	fi
+done
 
 
-echo -e "\nSetting up Database Environment Variables."
+echo -e "--------------------------------------------\n"
+echo -e "Now Creating Zensearch Tables,\nEnter your password for $DB_USER in $DB_NAME\n"
 
+mariadb -u "$DB_USER" -h "$DB_HOST" -D "$DB_NAME" -p < db.init.sql
 
+echo -e "Created Tables for Zensearch."
 
+echo -e "--------------------------------------------\n"
+echo -e "Setting up Database Environment Variables.\n"
 
 cat > .env << EOF
 DB_NAME=$DB_NAME
@@ -42,10 +61,7 @@ DB_USER=$DB_USER
 DB_PASS=$DB_PASS
 EOF
 
-echo -e "\nCreating Zensearch Tables..."
-
 echo -e "\n.env generated in the root directory.\n"
-
 echo -e "Done, Database and user created."
 
 
